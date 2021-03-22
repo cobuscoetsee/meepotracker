cdb0643364d922d3a65ccd33960c541b7b652db8
Coetsee Coetsee <cobus@matogen.com>
2021-03-22 18:57:30 +0200

    Added code for code missing due to incorrect reference in .gitignore

---
+++ MeepoRunner/GamePlay/Meepo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeepoRunner.GamePlay
+{
+    class Meepo
+    {
+        public Point position;
+        public float health;
+        public float manna;
+        public Boolean isBleeding = false;
+
+        
+        public Meepo()
+        {
+
+        }
+
+        public new string ToString => "Health:" + this.health + "Manna:" + this.manna + "Position:" + this.position.X + ";" + this.position.Y;
+
+        public Boolean isActive()
+        {
+
+            return true;
+        }
+    }
+}
